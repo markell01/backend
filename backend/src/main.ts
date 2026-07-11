9d31a744044837feadfb16e06bfc56944a5d63e3
markell01 <m.samoylov01@mail.ru>
2026-07-11 15:20:50 +0300

    change enableCors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'http://192.168.0.144:5173',
     credentials: true
   })
 
