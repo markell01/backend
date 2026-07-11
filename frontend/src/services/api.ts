ab1da3c9c75f4b355c15eece779b65c627b7446d
Artem <gerasimovatm1@gmail.com>
2026-07-11 15:21:40 +0300

    changed host and port

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://192.168.0.158:3000',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
